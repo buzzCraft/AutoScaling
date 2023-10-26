49754838d152292bc0d1a8ea87ae04a4676c3b1e
Buzz Lightyear <buzz@craft.com>
2023-10-26 13:16:20 +0200

    lets go

--- apps/Scaling/app/utils.py
+++ apps/Scaling/app/utils.py
@@ -122,13 +122,13 @@ def get_min_max(game:str)->List[int]:
 def get_current_players(game:str)->int:
     # Clean the response and return min and max
     resp = game_status(game=game)
-    players = resp[0]['values'][0][1]
+    players = int(resp[0]['values'][0][1])
     return players
 
 def get_running_servers()->int:
     # Clean the response and return min and max
     resp = server_status()
-    servers = resp[0]['values'][0][1]
+    servers = int(resp[0]['values'][0][1])
     return servers
 
 
