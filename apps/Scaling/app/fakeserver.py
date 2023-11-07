1b7a5e78c8ccde43ccc6df847f3fc9d3aed3e040
Buzz Lightyear <buzz@craft.com>
2023-11-07 09:03:23 +0100

    More bugs

--- apps/Scaling/app/fakeserver.py
+++ apps/Scaling/app/fakeserver.py
@@ -14,11 +14,13 @@ class FakeServer:
         logger.info(f"Scaling up {nr_of_servers} fake servers")
         for i in range(abs(nr_of_servers)):
             self.scale_up()
+            self.servers += 1
 
     def scale_down_servers(self, nr_of_servers):
         logger.info(f"Scaling down {nr_of_servers} fake servers")
         for i in range(abs(nr_of_servers)):
             self.scale_down()
+            self.servers -= 1
 
     def create_fakeserver(self):
         url = f"http://10.196.37.200:5000/fakeserver/create/{self.gamename}"
@@ -39,4 +41,5 @@ class FakeServer:
         return response.json()
     
     def get_servers(self):
+        logger.info(f"Getting number of servers: {self.servers} for {self.gamename}")
         return self.servers
