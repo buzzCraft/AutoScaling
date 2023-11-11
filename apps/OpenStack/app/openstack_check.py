82878057df30cec5c2a3f18c2e22747cd8d58a4e
Buzz Lightyear <buzz@craft.com>
2023-11-11 19:34:30 +0100

    mem leak..

--- apps/OpenStack/app/openstack_check.py
+++ apps/OpenStack/app/openstack_check.py
@@ -1,16 +1,21 @@
 from openstack import connection
 import os
-import logging
+# import logging
 
-logger = logging.getLogger('get_running_servers')
+# logger = logging.getLogger('get_running_servers')
+auth_url=os.getenv("OS_AUTH_URL")
+project_name=os.getenv("OS_PROJECT_NAME")
+username=os.getenv("OS_USERNAME")
+password=os.getenv("OS_PASSWORD")
 
 def get_running_servers():
+    running_servers = ""
     try:
         # Use a context manager to ensure the connection is properly closed
-        with connection.Connection(auth_url=os.getenv("OS_AUTH_URL"),
-                                   project_name=os.getenv("OS_PROJECT_NAME"),
-                                   username=os.getenv("OS_USERNAME"),
-                                   password=os.getenv("OS_PASSWORD"),
+        with connection.Connection(auth_url=auth_url,
+                                   project_name=project_name,
+                                   username=username,
+                                   password=password,
                                    user_domain_id='default',
                                    project_domain_id='default') as conn:
 
