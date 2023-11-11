09d7402f9ea83ccffa2b39b08a7433d9c6ce6667
Buzz Lightyear <buzz@craft.com>
2023-11-11 18:30:21 +0100

    trying to fix mem leek

--- apps/OpenStack/app/openstack_check.py
+++ apps/OpenStack/app/openstack_check.py
@@ -1,4 +1,3 @@
-
 from openstack import connection
 import os
 import logging
@@ -7,19 +6,22 @@ logger = logging.getLogger('get_running_servers')
 
 def get_running_servers():
     try:
-        conn = connection.Connection(auth_url=os.getenv("OS_AUTH_URL"),
-                                    project_name=os.getenv("OS_PROJECT_NAME"),
-                                    username=os.getenv("OS_USERNAME"),
-                                    password=os.getenv("OS_PASSWORD"),
-                                    user_domain_id='default',
-                                    project_domain_id='default')
+        # Use a context manager to ensure the connection is properly closed
+        with connection.Connection(auth_url=os.getenv("OS_AUTH_URL"),
+                                   project_name=os.getenv("OS_PROJECT_NAME"),
+                                   username=os.getenv("OS_USERNAME"),
+                                   password=os.getenv("OS_PASSWORD"),
+                                   user_domain_id='default',
+                                   project_domain_id='default') as conn:
 
-        servers = list(conn.compute.servers())
-        running_servers = [server for server in servers if server.status == 'ACTIVE']
-        return len(running_servers)-1
+            servers = list(conn.compute.servers())
+            running_servers = [server for server in servers if server.status == 'ACTIVE']
+            return len(running_servers) - 1
+
     except Exception as e:
         logger.error(e)
         return -1
+
     
 
 
