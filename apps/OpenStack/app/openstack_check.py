f7a9b63d1f1fc9987e3a7be5b5582fc530f4a469
Buzz Lightyear <buzz@craft.com>
2023-11-11 19:38:58 +0100

    more tests

--- apps/OpenStack/app/openstack_check.py
+++ apps/OpenStack/app/openstack_check.py
@@ -1,34 +1,35 @@
 from openstack import connection
 import os
-# import logging
 
-# logger = logging.getLogger('get_running_servers')
-auth_url=os.getenv("OS_AUTH_URL")
-project_name=os.getenv("OS_PROJECT_NAME")
-username=os.getenv("OS_USERNAME")
-password=os.getenv("OS_PASSWORD")
+# Assuming logging is properly configured
+import logging
+logger = logging.getLogger('get_running_servers')
+
+auth_url = os.getenv("OS_AUTH_URL")
+project_name = os.getenv("OS_PROJECT_NAME")
+username = os.getenv("OS_USERNAME")
+password = os.getenv("OS_PASSWORD")
 
 def get_running_servers():
-    running_servers = ""
     try:
-        # Use a context manager to ensure the connection is properly closed
-        with connection.Connection(auth_url=auth_url,
-                                   project_name=project_name,
-                                   username=username,
-                                   password=password,
-                                   user_domain_id='default',
-                                   project_domain_id='default') as conn:
+        conn = connection.Connection(auth_url=auth_url,
+                                     project_name=project_name,
+                                     username=username,
+                                     password=password,
+                                     user_domain_id='default',
+                                     project_domain_id='default')
+        
+        active_servers_count = 0
+        for server in conn.compute.servers():
+            if server.status == 'ACTIVE':
+                active_servers_count += 1
 
-            servers = list(conn.compute.servers())
-            running_servers = [server for server in servers if server.status == 'ACTIVE']
-            return len(running_servers) - 1
+        conn.close()
+        return active_servers_count - 1
 
     except Exception as e:
-        # logger.error(e)
+        logger.error("Error in get_running_servers: {}".format(e))
         return -1
-
-    
-
 
 if __name__ == "__main__":
     print(get_running_servers())
